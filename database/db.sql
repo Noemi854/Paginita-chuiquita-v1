e1cc66f6be036a24bf35c8b66fa6c9094e708392
Noemí Pino <noemisalome10@gmail.com>
2026-02-17 18:07:15 -0300

    subida de proyectitio

---
+++ database/db.sql
@@ -0,0 +1,14 @@
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO users (name, email) VALUES
+('John Doe', 'john.doe@example.com'),
+('Jane Smith', 'jane.smith@example.com'),
+('Alice Johnson', 'alice.johnson@example.com')
+;
+
+SELECT * FROM users;
