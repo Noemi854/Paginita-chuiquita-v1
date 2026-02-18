8819007f3184fb0f5a30566ae4bf30587bcfadd8
Noemí Pino <noemisalome10@gmail.com>
2026-02-18 16:34:30 -0300

    nuevos comandos sql para tareas

--- database/db.sql
+++ database/db.sql
@@ -1,3 +1,5 @@
+-- Crear tabla de usuarios
+
 CREATE TABLE users(
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
@@ -12,3 +14,22 @@ INSERT INTO users (name, email) VALUES
 ;
 
 SELECT * FROM users;
+
+-- Crear tabla de tareas
+
+CREATE TABLE tareas (
+    id SERIAL PRIMARY KEY,
+    titulo VARCHAR(255) NOT NULL,
+    descripcion TEXT,
+    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    fecha_limite DATE,
+    completada BOOLEAN DEFAULT FALSE,
+    prioridad INT DEFAULT 1 
+);
+
+INSERT INTO tareas (titulo, descripcion, fecha_limite, prioridad) VALUES
+('Comprar comestibles', 'Comprar leche, pan y huevos', '2026-03-01', 2),
+('Llamar al médico', 'Agendar cita para chequeo anual', '2026-07-05', 1),
+('Terminar proyecto', 'Completar el informe final del proyecto', '2026-07-10', 3);
+
+SELECT * FROM tareas;
